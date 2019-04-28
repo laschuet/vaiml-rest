ef2514477cea8e7c2e4ae6fbcf0206f9a2284130
laschuet <laschuet@users.noreply.github.com>
2019-04-28 20:54:23 +0200

    Add endpoint for returning available datasets

--- src/server.js
+++ src/server.js
@@ -1,20 +1,51 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 import sqlite from 'sqlite';
 
 const app = express();
 const host = 'localhost';
 const port = process.env.PORT || 4010;
 
+const readdir = async dir => {
+  return await new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, data) => {
+      err ? reject(err) : resolve(data);
+    });
+  });
+};
+
 app.use(bodyParser.json());
 
-app.get('/:db/contributions', async (req, res) => {
-  const dbname = req.params.db;
+app.get('/datasets', async (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  try {
+    const filenames = await readdir(path.join(process.cwd(), 'datasets'));
+    const data = filenames.map((filename, index) => ({
+      id: index,
+      name: filename,
+    }));
+    res.send(data);
+  } catch (err) {
+    /* eslint-disable no-console */
+    console.log(err);
+    /* eslint-enable */
+    res.sendStatus(500);
+  }
+});
+
+app.get('/datasets/:dataset/contributions', async (req, res) => {
+  const datasetName = req.params.dataset;
   const sql = 'SELECT * FROM contribution ORDER BY id';
 
   let data = [];
   try {
-    const db = await sqlite.open(`db/${dbname}`, sqlite.OPEN_READONLY);
+    const db = await sqlite.open(
+      `datasets/${datasetName}`,
+      sqlite.OPEN_READONLY,
+    );
     data = await db.all(sql);
     await db.close();
   } catch (err) {
